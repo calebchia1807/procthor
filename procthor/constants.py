5e9223166fc231b61e0bf03c0a16517d383c1195
calebchia1807 <calebchia1807@gmail.com>
2025-03-11 16:09:06 +0800

    working procthor, corrected the branch in constants.py; updated ai2thor version

--- procthor/constants.py
+++ procthor/constants.py
@@ -62,7 +62,7 @@ probability of choosing the largest remaining one.
 MIN_RECTANGLE_SIDE_SIZE = 0.5
 """The minimum rectangle size per side, in meters, that can be chosen."""
 
-PROCTHOR_INITIALIZATION = dict(branch="f0825767cd50d69f666c7f282e54abfe58f1e917", scene="Procedural")
+PROCTHOR_INITIALIZATION = dict(branch="391b3fae4d4cc026f1522e5acf60953560235971", scene="Procedural")
 "Base AI2-THOR initialization parameters for ProcTHOR."
 
 FLOOR_Y = 0
